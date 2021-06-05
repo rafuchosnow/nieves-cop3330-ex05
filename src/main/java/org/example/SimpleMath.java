2b3e6d4e8c2c88e25ee6a0b8a4ce94497bd572d4
rafuchosnow <rafuchosnow@gmail.com>
2021-06-05 16:46:21 -0400

    Initial Commit

---
+++ src/main/java/org/example/SimpleMath.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class SimpleMath {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the first number? ");
+        String firstString = input.nextLine();
+        double firstNumber = Double.parseDouble(firstString);
+
+        System.out.print("What is the second number? ");
+        String secondString = input.nextLine();
+        double secondNumber = Double.parseDouble(secondString);
+
+        double sum = firstNumber + secondNumber;
+        double subtraction = firstNumber - secondNumber;
+        double multiply = firstNumber * secondNumber;
+        double division = firstNumber / secondNumber;
+
+        String outputString = (firstNumber + " + " + secondNumber + " = " + sum + "\n" +
+                               firstNumber + " - " + secondNumber + " = " + subtraction + "\n" +
+                               firstNumber + " * " + secondNumber + " = " + multiply + "\n" +
+                               firstNumber + " / " + secondNumber + " = " + division + "\n");
+
+        System.out.println(outputString);
+    }
+}
